31ad881e7d3e3bf4c4559d857cfae68658513472
gsspdev <dev@gssp.io>
2022-09-30 16:20:34 -0500

    Initial Commit

---
+++ avfucking-audio/avfucking_audioApp.swift
@@ -0,0 +1,17 @@
+//
+//  avfucking_audioApp.swift
+//  avfucking-audio
+//
+//  Created by gossip on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct avfucking_audioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
